641f5221c15dd5d1c1293fb7b29a7d86a40c999d
Chibike-267 <paulinuschukwubuikem98@gmail.com>
2023-12-25 12:07:25 +0100

    my project

---
+++ src/assets/index.ts
@@ -0,0 +1,31 @@
+import naira from "./naira.svg";
+import Artistic from "./Artistic.jpg";
+import Artistic1 from "./Artistic1.jpg";
+import avatar from "./avatar.png";
+import AvatarImage from "./AvatarImage.jpg";
+import backgroundPic from "./backgroundPic.svg";
+import Chibike from "./Chibike.jpg";
+import logo from "./logo.svg";
+import profilePic from "./profilePic.jpeg";
+import search from "./search.svg";
+import square from "./square.png";
+import success from "./success.svg";
+import Visa from "./Visa.svg";
+import marisPic from "../assets/dashboard/marisPic.png";
+
+export {
+naira,
+Artistic,
+Artistic1,
+avatar,
+AvatarImage,
+backgroundPic,
+Chibike,
+logo,
+profilePic,
+search,
+square,
+success,
+Visa,
+marisPic,
+}
